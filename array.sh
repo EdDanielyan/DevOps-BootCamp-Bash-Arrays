cf6bf898e57a2cabe70781a5d9b500148901c9e5
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-10 13:00:18 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 # Place your code here
+
+chars=( {a..z} )
+for ((i=0; i<$1; i++))
+do
+  mkdir "folder_${chars[i]}"
+done
+
+if [[ $1 == 1 ]] 
+then 
+    echo -n "$1 folder created: "
+else
+    echo -n "$1 folders created: "
+fi
+
+ls -d folder_*  | tr ":\n" ", " | sed 's/. $//'
+
